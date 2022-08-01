911671317474b4458168a6f9e8600b201c67d398
Renee Bogdany <Renee.Bogdany@libertymutual.com>
2022-08-01 17:16:43 -0400

    added search bar and submit button and displayed information from search bar on click

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import React, { useState } from "react";
+
 function App() {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
+
         <p>
           Search That Lyric
         </p>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+            id="search-input"
+          />
+          <Button onClick={() => { setInputText(document.getElementById("search-input").value) }} variant="outline-success">Search</Button>
+        </Form>
+
+        <p>{inputText}</p>
+
+
 
       </header>
     </div>
