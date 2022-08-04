24c86548d13e4c6b77eb6cd1ec09caf3219e4e8b
Viktoriya Loshkarova <viktoriya.loshkarova@libertymutual.com>
2022-08-04 12:34:03 -0400

    switched the loading gif

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Song from "./components/song/song.js"
-import spinner from "./spinner.gif"
+import spinner from "./loading.gif"
 
 export default function Api(props) {
 
