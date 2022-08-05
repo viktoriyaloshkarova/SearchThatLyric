3f3e98d6069172139d8a953106ee012f9db9e772
Viktoriya Loshkarova <viktoriya.loshkarova@libertymutual.com>
2022-08-05 09:35:16 -0400

    Tried fixing css for cards

--- src/components/song/song.js
+++ src/components/song/song.js
@@ -5,7 +5,7 @@ export default function Song(props) {
     console.log(props)
 
     return (
-        <div>
+        <div class="cards">
             <div class="card">
                 <p class="title">{props.song.track_name} </p>
             <p class="artist">By: {props.song.artist_name}</p>
